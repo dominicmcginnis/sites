83031d6fdd2d6ee62e0ba866df46072795613b09
Dominic McGinnis <dominicmcginnis@gmail.com>
2013-11-27 13:51:29 -0800

    Early foray into building sites ~2005/2006 timeframe. Just adding to git

---
+++ moonMagic/src-www/js/menu.js
@@ -0,0 +1,19 @@
+function drawMenu() {
+
+var outputHtml = '<table align="center" cellspacing="0" cellpadding="0" border="0" bgcolor="#2F97FF" width="850">' +
+     '<tr><td align="right"><a href="about.html"><img src="../images/moonmagic.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"><a href="about.html"><b>About</b></a></td>' +
+	 '<td>&nbsp;</td><td>&nbsp;</td><td align="right"><a href="reviews.html"><img src="../images/thumbs_up.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"><a href="reviews.html"><b>Reviews</b></a></td><td>&nbsp;</td>' +
+     '<td>&nbsp;</td> <td align="right"><a href="music.html"><img src="../images/cd2.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"><a href="music.html"><b>Music</b></a></td><td>&nbsp;</td>' +
+     '<td>&nbsp;</td><td align="right"><a href="faq.html"><img src="../images/question.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"> <a href="faq.html"><b>FAQ</b></a></td><td>&nbsp;</td>' +
+     '<td>&nbsp;</td><td align="right"><a href="contact.html"><img src="../images/quill.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"><a href="contact.html"><b>Contact</b></a></td><td>&nbsp;</td>' +
+     '<td>&nbsp;</td><td align="right"><a href="main.html"><img src="../images/house.jpg" width="30" height="30" border="0"></a></td>' +
+     '<td align="left"> <a href="main.html"><b>Home</b></a></td>' +
+     '</tr></table>';
+
+document.write(outputHtml);
+}
